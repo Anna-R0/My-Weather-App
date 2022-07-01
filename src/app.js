9c139476ef0214bd118aaad48a4fa514213f7d69
Anna <eviltwin007@yandex.ru>
2022-06-30 17:45:30 -0700

    Added API to the forecast

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -49,9 +50,9 @@ function displayForecast() {
 }
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "26b1e02ac747e8bd3b3d64e49e0c66c3";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apikey}&units=metric`;
-  console.log(response.data);
+  let apiKey = "ce3ce8670f79138318ddc69624ab8399";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -76,10 +77,10 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  getForecast(response.data.coords);
+  getForecast(response.data.coord);
 }
 function search(city) {
-  let apiKey = "26b1e02ac747e8bd3b3d64e49e0c66c3";
+  let apiKey = "ce3ce8670f79138318ddc69624ab8399";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayTemperature);
@@ -119,5 +120,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Milan");
 displayForecast();
